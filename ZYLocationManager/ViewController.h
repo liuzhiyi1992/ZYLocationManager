9b049c19d5b8626af8218abfc26ace602c94880b
liuzhiyi1992 <55137840@qq.com>
2016-07-14 18:44:32 +0800

    Initial Commit

---
+++ ZYLocationManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYLocationManager
+//
+//  Created by lzy on 16/7/14.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZYLocationManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZYLocationManager
+//
+//  Created by lzy on 16/7/14.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
